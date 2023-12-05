1e0c8fce33ad87434b519fd5dc4ffa764dd883ef
Scott <scottyunho239@gmail.com>
2023-12-05 00:04:54 -0500

    Initial commit

--- backend/routes/franchise.js
+++ backend/routes/franchise.js
@@ -17,7 +17,25 @@ const utils = require('../utils')
 var sql_queries = require('../sql')
 
 router.get('/:franchise/countries/', (req, res) => {
-    // 4.3.2 Parse parameters and connect to Snowflake a return query response    
+    // 4.3.2 Parse parameters and connect to Snowflake a return query response
+    const franchise = req.params.franchise
+    const startdate = utils.parseDate(req.query.start) ?? utils.defaultStartDate();
+    const enddate = utils.parseDate(req.query.end) ?? utils.defaultEndDate();
+
+    console.log('start: ' + startdate + ', end: ' + enddate);
+
+    connection.execute({
+        sqlText: sql_queries.top_10_countries,
+        binds: [franchise, startdate, enddate],
+        complete: (err, stmt, rows) => {
+            if (err) {
+                console.error('Unable to retrieve order data', err);
+                res.status(500).json({ error: 'Unable to retrieve order data' });
+            } else {
+                res.status(200).json(rows);
+            }
+        },
+    });
 });
 
 router.get('/:franchise/trucks/:truckbrandname/sales_topitems', (req, res) => {
